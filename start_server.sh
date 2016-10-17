f7c79c8d7803b57efa1fc06b4b73ece3a80c0582
Neil Swainston <neil.swainston@manchester.ac.uk>
2016-10-17 11:55:11 +0100

    Simplified Docker script.

--- start_server.sh
+++ start_server.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
-
-if [ "$(uname)" == "Darwin" ]; then
-	eval "$(docker-machine env default)"
-fi
-
 docker build -t codongenie .
-docker run --detach --publish=80:80 codongenie
+docker run -d -p 80:80 codongenie
